31c4acdd8c8fa229b344325da471a1d7f6bd4584
Granted <anjishnu.04032007@gmail.com>
2022-12-27 14:01:20 +0530

    Added Fonts

---
+++ .config/autostart.sh
@@ -0,0 +1,8 @@
+nitrogen --restore >> ~/.config/AutoStart.log 
+picom &
+/usr/lib/xfce-polkit/xfce-polkit &
+flameshot &
+dunst &
+sxhkd -c /home/granted/.config/sxhkd/sxhkdrc-dwm &
+discord &
+copyq &
